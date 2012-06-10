6fc37532b867b4dd138190336451ca688e656473
Ryouzanki <ryouzanki@hotmail.fr>
2012-06-10 19:38:44 +0200

    suppression des accents dans 2.m, ajout d une figure pour le ISEF et quelques commentaires

--- matlab/2.m
+++ matlab/2.m
@@ -2,18 +2,18 @@ L = 1; % nombre de vues moyennees
 N = 500; % nb de valeur
 Nc = 80; % nombre de classe
 
-%Générer l'histogramme normalisé
+%Generer l'histogramme normalise
 bruit_mult = gamrnd(L, 1/L, 1, N);
 [hist abs] = hist(bruit_mult,Nc);
 bruit_th = gampdf(abs, L, 1/L);
 Nc = 50; 
 dx = (max(bruit_mult)-min(bruit_mult))/Nc;
-%Affichage de l'histogramme normalisé
+%Affichage de l'histogramme normalise
 figure('name', 'Bruit multiplicatif n(x)')
 bar(abs, hist/(dx*length(bruit_mult)));
-title('Histogramme normalisé et fonction de répartition de loi Gamma')
+title('Histogramme normalise et fonction de repartition de loi Gamma')
 hold on
-%Affichage de la fonction de répartition théorique
+%Affichage de la fonction de repartition theorique
 plot(abs, bruit_th, 'r');
 
 n = bruit_mult;
